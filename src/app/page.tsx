ef19045a93d017a3a96e1c141979191192946dee
chilleno <antonion.gonzalezf@gmail.com>
2023-07-22 21:57:32 -0400

    Update page.tsx
    - implements footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Avatar from "./Components/Avatar";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Avatar />
+      <Footer />
     </main>
   )
 }
