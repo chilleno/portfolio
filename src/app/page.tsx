058260eee66ecd5578e47c133671d3e439e9ea02
chilleno <antonion.gonzalezf@gmail.com>
2023-07-23 18:37:46 -0400

    Update page.tsx
    - add dark mode compatibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,41 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Header from "./Components/Header";
 import Avatar from "./Components/Avatar";
 import Links from "./Components/Links";
 import Footer from "./Components/Footer";
 
+let className = {
+  main: {
+    dark: "min-h-screen max-h-screen bg-black",
+    light: "min-h-screen max-h-screen bg-white",
+  }
+}
+
 export default function Home() {
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (typeof localStorage.darkMode !== 'undefined') {
+      setDarkMode(localStorage.darkMode === "true");
+    }
+  }, []);
+
   return (
-    <main className="max-h-screen">
-      <Header />
-      <Avatar />
-      <Links />
-      <Footer />
+    <main className={darkMode ? className.main.dark : className.main.light}>
+      <Header
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
+      <Avatar
+        darkMode={darkMode}
+      />
+      <Links
+        darkMode={darkMode}
+      />
+      <Footer
+        darkMode={darkMode}
+      />
     </main>
   )
 }
