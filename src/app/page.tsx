7bcc74952d3e485393ef6b69bfd6482ef0abdd6f
chilleno <antonion.gonzalezf@gmail.com>
2023-07-22 21:49:51 -0400

    Update page.tsx
    - implements Avatar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FixedHeader from "./Components/FixedHeader";
+import Avatar from "./Components/Avatar";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <FixedHeader />
+      <Avatar />
     </main>
   )
 }
