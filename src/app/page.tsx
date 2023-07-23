31ced5f40d860884af9924630c67a22ea3f437e5
chilleno <antonion.gonzalezf@gmail.com>
2023-07-23 14:34:04 -0400

    Update page.tsx
    - remove min-h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="max-h-screen">
       <Header />
       <Avatar />
       <Links />
