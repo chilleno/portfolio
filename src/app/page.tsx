888b1bb3e190f0a71aaa5aca1a3a787d0f6d581d
chilleno <antonion.gonzalezf@gmail.com>
2023-07-23 14:31:30 -0400

    Update page.tsx
    - implements Links component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./Components/Header";
 import Avatar from "./Components/Avatar";
+import Links from "./Components/Links";
 import Footer from "./Components/Footer";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <Avatar />
+      <Links />
       <Footer />
     </main>
   )
