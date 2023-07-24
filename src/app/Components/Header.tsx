7d8645a4720966bf00412e0606b641f4d51322e4
chilleno <antonion.gonzalezf@gmail.com>
2023-07-23 20:36:48 -0400

    Update Header.tsx
    - add responsive
    - add bg color

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -8,8 +8,8 @@ interface HeaderProps {
 
 let className = {
     main: {
-        dark: "fixed top-0 left-0 w-full text-white py-4 px-8 flex content-center justify-center gap-96",
-        light: "fixed top-0 left-0 w-full text-black py-4 px-8 flex content-center justify-center gap-96",
+        dark: "fixed top-0 left-0 w-full text-white bg-black py-4 px-8 flex content-center justify-center xl:gap-96 sm:gap-12 z-50",
+        light: "fixed top-0 left-0 w-full text-black bg-white py-4 px-8 flex content-center justify-center xl:gap-96 sm:gap-12 z-50",
     }
 }
 
@@ -22,7 +22,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, setDarkMode }) => {
 
     return (
         <header className={darkMode ? className.main.dark : className.main.light}>
-            <h1 className="text-md font-bold mr-40">Antonio González</h1>
+            <h1 className="text-md font-bold xl:mr-40">Antonio <span className="xl:inline lg:inline md:inline sm:hidden">González</span></h1>
             <div className="flex gap-5 text-md">
                 <a href="/projects" className="hover:font-bold">Projects</a>
                 <a href="/cv" className="hover:font-bold">CV</a>
