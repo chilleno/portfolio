37f007333ae84d6e1383c96df214638992c059d6
chilleno <antonion.gonzalezf@gmail.com>
2023-07-29 02:28:16 -0400

    Create fetchCommitData.tsx
    - add function to get data from github public api

---
+++ src/app/projects/util/fetchCommitData.tsx
@@ -0,0 +1,15 @@
+const fetchCommitData = async (username: string, repoName: string): Promise<any> => {
+    try {
+        const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/commits`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        const commitsData = response.json();
+        return commitsData;
+    } catch (error) {
+        console.error('Error fetching commit data:', error);
+        return {};
+    }
+};
+
+export default fetchCommitData;
