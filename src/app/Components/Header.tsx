0e56ed97fc7371e1e07d1d7e53ca193cdaea3c23
chilleno <antonion.gonzalezf@gmail.com>
2024-01-06 14:19:59 -0300

    feat: add home link

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -24,6 +24,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, setDarkMode }) => {
         <header className={darkMode ? className.main.dark : className.main.light}>
             <a href="/" className="hover:font-bold hover:cursor-pointer"><h1 className="text-md font-bold xl:mr-40 lg:mr-10">Antonio <span className="xl:inline lg:inline md:inline sm:hidden">González</span></h1></a>
             <div className="flex gap-5 text-md">
+                <a href="/" className="hover:font-bold xl:min-w-[50px] lg:min-w-[50px]">Home</a>
                 <a href="/cv" className="hover:font-bold xl:min-w-[30px] lg:min-w-[30px]">CV</a>
                 <a href="/contact" className="hover:font-bold xl:min-w-[70px] lg:min-w-[70px]">Contact</a>
                 <h1 className="hover:font-bold hover:cursor-pointer xl:min-w-[70px] lg:min-w-[70px]">
